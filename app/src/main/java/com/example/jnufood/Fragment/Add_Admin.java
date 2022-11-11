5641ebfe6d367e6c8921f441a166cc769e5c4064
MD Mojahid AL Tarif <mojahidaltarif78@gmail.com>
2022-11-12 00:15:01 +0600

    aaaaaaa

--- app/src/main/java/com/example/jnufood/Fragment/Add_Admin.java
+++ app/src/main/java/com/example/jnufood/Fragment/Add_Admin.java
@@ -1,5 +1,6 @@
 package com.example.jnufood.Fragment;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,8 +8,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.example.jnufood.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,7 +22,9 @@ import com.example.jnufood.R;
  * create an instance of this fragment.
  */
 public class Add_Admin extends Fragment {
-
+    EditText name, email, mobile;
+    TextView add_btn;
+    ProgressBar progressBar;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -57,10 +65,47 @@ public class Add_Admin extends Fragment {
         }
     }
 
+    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add__admin, container, false);
+        View view = inflater.inflate(R.layout.fragment_add__admin, container, false);
+        name = view.findViewById(R.id.ada_name);
+        mobile = view.findViewById(R.id.ada_mobile);
+        email = view.findViewById(R.id.ada_email);
+        add_btn = view.findViewById(R.id.ada_add_btn);
+        progressBar = view.findViewById(R.id.ada_progress);
+        add_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                add_btn.setVisibility(View.GONE);
+                progressBar.setVisibility(View.VISIBLE);
+                if (name.getText().toString().isEmpty()) {
+                    showError(name, "Enter Name");
+                    add_btn.setVisibility(View.VISIBLE);
+                    progressBar.setVisibility(View.GONE);
+                }else if(mobile.getText().toString().length()<11){
+                    showError(mobile,"Invalid Mobile Number");
+                    add_btn.setVisibility(View.VISIBLE);
+                    progressBar.setVisibility(View.GONE);
+                }else if(email.getText().toString().isEmpty()){
+                    showError(email,"Please Enter Email");
+                    add_btn.setVisibility(View.VISIBLE);
+                    progressBar.setVisibility(View.GONE);
+                }else{
+                    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://jnufood-default-rtdb.firebaseio.com/");
+
+                }
+            }
+        });
+
+
+        return view;
+    }
+
+    private void showError(EditText input, String s) {
+        input.setError(s);
+        input.requestFocus();
     }
 }
